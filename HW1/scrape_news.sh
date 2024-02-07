b3c22dce7b9b1d3621228f805bb7a8a67cf815ba
lavieled <158300455+lavieled@users.noreply.github.com>
2024-02-07 13:28:58 +0200

    Update scrape_news.sh

--- HW1/scrape_news.sh
+++ HW1/scrape_news.sh
@@ -2,15 +2,21 @@
 # scrape news bash script
 # 319046504 laviel@campus.technion.ac.il lavie lederman
 # 206159527 shahary@campus.technion.ac.il shahar yankelovich
-# set working params
-URL="https://www.ynetnews.com/category/3082"
+URL="https://www.ynetnews.com/category/3082" # site to scan
 data=$(wget --no-check-certificate -O - $URL 2>/dev/null)
 
 articles=$(echo "$data" | 
-			grep -oP "https://(www.)?ynetnews.com/article/[0-9a-zA-Z]+" | sort | uniq |
-
+	grep -oP "https://(www.)?ynetnews.com/article/[0-9a-zA-Z]+" | sort | uniq)
+#choose all relevant links, sort and delete duplicates 
 while IFS= read -r line; do
-wget --no-check-certificate -O - $line 2>/dev/null |
-echo "$line, Netanyahu, " #" $(grep -o "Netanyahu" | wc -l)"
-done
-) #nope
+article_data=$(wget --no-check-certificate -O - $line 2>/dev/null)
+bibi=$(echo "$article_data" | grep -o "Netanyahu" | wc -l)
+gantz=$(echo "$article_data" | grep -o "Gantz" | wc -l)
+#for each links, go to article and count num appearces of chosen politicians
+if [[ ("$bibi" -eq 0) && ("$gantz" -eq 0) ]]; then
+	echo "$line, -"
+else 
+echo "$line, Netanyahu, $bibi, Gantz, $gantz"
+fi
+#print to screen results
+done <<< "$articles"
