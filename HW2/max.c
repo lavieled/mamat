76c8d7deb019a21221cf7408f75da18113b8c346
lavieled <158300455+lavieled@users.noreply.github.com>
2024-02-14 16:37:15 +0200

    Update max.c

--- HW2/max.c
+++ HW2/max.c
@@ -1,12 +1,17 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void mean(FILE *f);
+#define MAX_GRADE 100
+#define MIN_GRADE 0
+//definitions for grade range
+void max(FILE *f);
 
 int main(int argc, char **argv) {
     FILE *f;
-
+    //check input method
     if(argc == 1 || !strcmp("-", argv[1])){
         f = stdin;
     }
@@ -18,27 +23,33 @@ int main(int argc, char **argv) {
         fprintf(stderr, "File not found: \"%s\"\n", argv[1]);
         return 1;
     }
-    mean(f);
+    max(f);//prints max
     return 0;
 }
-void mean(FILE *f){
+void max(FILE *f){
     int grade;
     int retval;
     int max = -1;
+    int line = 1;
     while(1){
         retval = fscanf(f, "%d", &grade);
         if(retval == EOF){
             break;//finished
         }
-        else if(retval != 1){
-            //error
+        else if(retval != 1){//error
+            
             fprintf(stderr, "Error: not a number\n");
             exit(1);
         }
-        if(grade > max){
+        if (grade < MIN_GRADE || grade > MAX_GRADE) {//not in grade range
+            fprintf(stderr, "Error at line %d: grade %d invalid\n", line, grade);
+            exit(1);
+        }
+        if(grade > max){//update max
             max = grade;
         }
+        line++;
 
     }
-    printf("%.2lf\n", max);
+    printf("%.d\n", max);
 }
