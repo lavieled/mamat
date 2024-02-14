f12f61393bc04feace1bde3a2d868b14e841a6ec
lavieled <158300455+lavieled@users.noreply.github.com>
2024-02-14 15:59:36 +0200

    Update median.c

--- HW2/median.c
+++ HW2/median.c
@@ -11,52 +11,56 @@ int median(char *argv, int num_elements);
 int compare(const void *a, const void *b);
 
 int main(int argc, char **argv) {
-    FILE *f;
-
-    if(argc == 1 || !strcmp("-", argv[1])){
+    if (argc == 1 || !strcmp("-", argv[1])) {
         f = stdin;
+    } else {
+        f = fopen(argv[1], "r");
     }
-    else{
-        fopen(argv[1], "r");
-    }
-    //check for errors
-    if(!f){
+
+    // Check for errors
+    if (!f) {
         fprintf(stderr, "File not found: \"%s\"\n", argv[1]);
         return 1;
     }
 
-    return median(argv[1], argc);
+    int result = median(argv[1], argc);
+    printf("Median: %d\n", result);
+
+    fclose(f);
+    return 0;
 }
 
-int median(char *argv, int num_elements){
-    int *arr = (int *)malloc(num_elements*sizeof(int));
+int median(char *argv, int num_elements) {
+    int *arr = (int *)malloc(num_elements * sizeof(int));
     int grade;
     int retval;
-    int line = 1; 
+    int line = 0;
+
     while (1) {
         retval = fscanf(f, "%d", &grade);
         if (retval == EOF) {
             break; /* Finished */
-        } 
-        else if (retval != 1) { /* Error */
-        fprintf(stderr, "Error: not a number\n");
-        exit(1);
-    }
-/* Find bin */
-    if(grade < MIN_GRADE || grade > MAX_GRADE){
-        fprintf(stderr, "Error at line %d: grade %d invalid\n", line, grade);
-        exit(1);
-    }
-     else{
-        arr[line-1] = grade;
-    }
-}
-    int median;
-    qsort(arr, num_elements, sizeof(int), compare);
-    median = arr[((num_elements+1)/2)];
-    return median;
+        } else if (retval != 1) { /* Error */
+            fprintf(stderr, "Error: not a number\n");
+            exit(1);
+        }
 
+        /* Find bin */
+        if (grade < MIN_GRADE || grade > MAX_GRADE) {
+            fprintf(stderr, "Error at line %d: grade %d invalid\n", line, grade);
+            exit(1);
+        } else {
+            arr[line++] = grade;
+        }
+    }
+
+    int median;
+    qsort(arr, line, sizeof(int), compare);
+    median = arr[(line + 1) / 2];
+    free(arr);
+    return median;
 }
+
 int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+    return (*(int *)a - *(int *)b);
 }
