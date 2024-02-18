78d4554f7e4b8149224313b625af5045edd33858
YanShahar <158297968+YanShahar@users.noreply.github.com>
2024-02-18 23:45:07 +0200

    Update calc_stat.sh
    write the first condition - if the number of arguments is valid

--- HW2/calc_stat.sh
+++ HW2/calc_stat.sh
@@ -1,1 +1,9 @@
 # enter the bash code
+#!bin/bash
+# First we check the input's validity
+if [[ ! "$1" =~ ^[0-9]$ || ! $# -eq 1 ]] {
+  #Input invalid
+  error="Wrong number of arguments";
+  error>2;
+  exit(1);
+}
