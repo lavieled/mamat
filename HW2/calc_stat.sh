86308d1565f27cd3719ec8e2bb1ebbcdd607f62a
YanShahar <158297968+YanShahar@users.noreply.github.com>
2024-02-25 22:58:44 +0200

    Update calc_stat.sh
    continue the reading info part

--- HW2/calc_stat.sh
+++ HW2/calc_stat.sh
@@ -6,23 +6,22 @@ if [[ ! "$1" =~ ^[0-9]$ || ! $# -eq 1 ]]; then
   error="Wrong number of arguments";
   error>$2;
   exit(1);
-fi
 # Second, we check if there is a valid file inside the repo
-if [[ ]]; then
+elif [[ ! -e "$1.txt" ]]; then
   #There is no such file
   error="Course not found";
   error>$2;
   exit(1);
-fi
-# Third, we check if a repository with the wanted name already exists
-if [[ ]]; then
-# There is a repo already, deleting it
-  rm -rf ""
-fi
-# Creating the new repository
-dir="$1_stat"
-# Reading the data from the recently created file 
+else
+# We create a new file with the statistics
+# And read the data from the input/file 
 # and produce a histogram into a file
-
+  res_dir="$1_stat"
+  grades_file="$1_stat/grades.txt"
+  course_statistics="$1_stat/statistics.txt"
+# We recreate the directory empty
+  rm -rf "$res_dir"; mkdir "$res_dir"
+  
 # Creating a file that combines all the statistics:
 # Avg, Median, Min, max - devided by a tab
+fi
