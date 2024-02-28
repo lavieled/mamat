a12a59e87b04a53a5b3d36bc486412fd06342e4b
lavieled <158300455+lavieled@users.noreply.github.com>
2024-02-28 17:54:40 +0200

    Update calc_stat.sh

--- HW2/calc_stat.sh
+++ HW2/calc_stat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # First we check the input's validity - that it is a course number with 6 digits
-# And that it contains more than one line
 if [[ ! "$1" =~ ^[0-9]{6}$ || ! $# -eq 1 ]]; then
   #Invalid input
   echo "Wrong number of arguments" >&2
@@ -20,23 +19,21 @@ else
 #************************************ Maybe not necessery grades_file="$1_stat/grades.txt"
 #********************************************** course_statistics="$1_stat/statistics.txt"
 # Compiling the histogram code
-  gcc -g -Wall -o "histogram.exe" "histogram.c"
-  ./histogram.exe "histogram.txt" "$1_stat/grades.txt"
+  gcc -g -Wall -o "histogram.exe" "hist.c"
+  ./histogram.exe < "$1.txt" > "histogram.txt"
 # Creating a file that combines all the statistics:
 # Avg, Median, Min, max - devided by a tab
 #calculate course statistics
-  #init statistics file
-  course_statistics="$1_stat/statistics.txt"
   #create array of files to compile and run for the statistics
   source_files=("mean.c" "median.c" "min.c" "max.c")
   #loop to run and add the stats each program
   for file in "${source_files[@]}"; do
   #get function from array
-  func_name=$(basename "$file" .c)
+  func_name=$(basename "$file")
   #compile the current function
   gcc -o "$func_name" "$file"
   #run the func and direct output to statistics file
-  ./"$func_name" >> "$course_statistics"
-  rm "$func_name"
-  done
+  ./"$func_name" < "$1.txt" >> "statistics.txt"
+  #rm "$func_name"
+  done
 fi
