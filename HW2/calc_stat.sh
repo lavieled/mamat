960480191e79cc386215122f36042dd25a0f7d75
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-12 19:12:27 +0200

    Update calc_stat.sh

--- HW2/calc_stat.sh
+++ HW2/calc_stat.sh
@@ -1,6 +1,13 @@
+#319046504 laviel@campus.technion.ac.il lavie lederman
+#206159527 shahary@campus.technion.ac.il shahar yankelovich
+
 #!/bin/bash
+  res_dir="$1_stat"
+# We recreate the directory empty
+  rm -rf "$res_dir"; mkdir "$res_dir"
+  #touch command needed
 # First we check the input's validity - that it is a course number with 6 digits
-if [[ ! "$1" =~ ^[0-9]{6}$ || ! $# -eq 1 ]]; then
+if [[ ! "$1" =~ ^[0-9]{6}$ || ! "$#" -eq 1 ]]; then
   #Invalid input
   echo "Wrong number of arguments" >&2
   exit 1
@@ -13,14 +20,11 @@ else
 # We create a new file with the statistics
 # Then, read the data from the input/file 
 # And produce a histogram into a file
-  res_dir="$1_stat"
-# We recreate the directory empty
-  rm -rf "$res_dir"; mkdir "$res_dir"
-#************************************ Maybe not necessery grades_file="$1_stat/grades.txt"
-#********************************************** course_statistics="$1_stat/statistics.txt"
+
 # Compiling the histogram code
-  gcc -g -Wall -o "histogram.exe" "hist.c"
+  gcc -g -Wall "hist.c" -o "histogram.exe" 
   ./histogram.exe < "$1.txt" > "$1_stat/histogram.txt"
+  # rm "histogram.exe"
 # Creating a file that combines all the statistics:
 # Avg, Median, Min, max - devided by a tab
 #calculate course statistics
@@ -31,10 +35,9 @@ else
   #get function from array
   func_name=$(basename "$file" .c)
   #compile the current function
-  gcc -g -o "${func_name}" "$file"
+  #gcc -g -Wall -o "${func_name}" "$file"
   #run the func and direct output to statistics file
-  ##################################################################eroor line 37
-  ./"${func_name}" < "$1.txt" >> "$1_stat/statistics.txt" 2>&1
-  rm "${func_name}"
+  ./"${func_name}.exe" < "$1.txt" >> "$1_stat/statistics.txt" 2>&1
+  #rm "${func_name}"
   done
 fi
