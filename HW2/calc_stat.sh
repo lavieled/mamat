290bdaa200f2358003285329cd3c83ad1d84d37d
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-13 14:15:10 +0200

    Update calc_stat.sh

--- HW2/calc_stat.sh
+++ HW2/calc_stat.sh
@@ -5,7 +5,9 @@
   res_dir="$1_stat"
 # We recreate the directory empty
   rm -rf "$res_dir"; mkdir "$res_dir"
-  #touch command needed
+ #create the files we will pass the stats to
+  touch "$1_stat"/histogram.txt
+  touch "$1_stat"/statistics.txt
 # First we check the input's validity - that it is a course number with 6 digits
 if [[ ! "$1" =~ ^[0-9]{6}$ || ! "$#" -eq 1 ]]; then
   #Invalid input
@@ -18,26 +20,29 @@ elif [[ ! -e "$1.txt" ]]; then
   exit 1
 else
 # We create a new file with the statistics
+ #create the files we will pass the stats to
 # Then, read the data from the input/file 
 # And produce a histogram into a file
 
 # Compiling the histogram code
-  gcc -g -Wall "hist.c" -o "histogram.exe" 
-  ./histogram.exe < "$1.txt" > "$1_stat/histogram.txt"
+  #gcc -g -Wall "hist.c" -o "hist.exe" 
+  ./hist.exe -nbins 10 < "$1.txt" > "histogram.txt"
   # rm "histogram.exe"
 # Creating a file that combines all the statistics:
 # Avg, Median, Min, max - devided by a tab
 #calculate course statistics
   #create array of files to compile and run for the statistics
-  source_files=("mean.c" "median.c" "min.c" "max.c")
+  #source_files=("mean.exe" "median.exe" "min.exe" "max.exe")
   #loop to run and add the stats each program
-  for file in "${source_files[@]}"; do
+  for file in ("mean.exe" "median.exe" "min.exe" "max.exe"); do
   #get function from array
-  func_name=$(basename "$file" .c)
+  #func_name=$(basename "$file")
   #compile the current function
   #gcc -g -Wall -o "${func_name}" "$file"
   #run the func and direct output to statistics file
-  ./"${func_name}.exe" < "$1.txt" >> "$1_stat/statistics.txt" 2>&1
+  ./"${file}" < "$1.txt" >> "statistics.txt" #2>&1
   #rm "${func_name}"
   done
+  #mv "statistics.txt" "${course_num}_stat"
+  #mv "histogram.txt" "${course_num}_stat"
 fi
