e01fba972054010a74cd1febe06e1b9d6f82554d
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-26 17:59:29 +0200

    Update string.h

--- HW6/string.h
+++ HW6/string.h
@@ -1,1 +1,37 @@
+//Lavie Lederman 319046504
+//Shahar Yankelovich 206159527
+//
 
+#ifndef STRING_H
+#define STRING_H
+
+
+#include <cstring> // Include necessary header files
+
+class String {
+private:
+    char *data; // Pointer to character array holding the string data
+    size_t length; // Length of the string
+
+public:
+    // Constructors and destructor
+    String(); // Default constructor
+    String(const String &str); // Copy constructor
+    String(const char *str); // Constructor from C-style string
+    ~String(); // Destructor
+
+    // Overloaded assignment operators
+    String& operator=(const String &rhs);
+    String& operator=(const char *str);
+
+    // Comparison functions
+    bool equals(const String &rhs) const;
+    bool equals(const char *rhs) const;
+
+    // Utility functions
+    void split(const char *delimiters, String **output, size_t *size) const;
+    int to_integer() const;
+    String trim() const;
+};
+
+#endif // STRING_H
