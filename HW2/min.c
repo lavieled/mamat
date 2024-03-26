c38a1eb0e4610496fd80bdd8cdf5af409bab9b9d
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-26 17:49:38 +0200

    Update min.c

--- HW2/min.c
+++ HW2/min.c
@@ -12,12 +12,12 @@ void min(FILE *f);
 
 int main(int argc, char **argv) {
     FILE *f;
-
+    //decide input method
     if(argc == 1 || !strcmp("-", argv[1])){
         f = stdin;
     }
     else{
-        fopen(argv[1], "r");
+        f = fopen(argv[1], "r");
     }
     //check for errors
     if(!f){
@@ -33,7 +33,7 @@ void min(FILE *f){
     int min = 101;
     int line = 1;
     while(1){
-        retval = fscanf(f, "%d", &grade);
+        retval = fscanf(f, "%d", &grade);//read number from file
         if(retval == EOF){
             break;//finished
         }
@@ -41,16 +41,16 @@ void min(FILE *f){
             //error
             fprintf(stderr, "Error: not a number\n");
             exit(1);
-        }
+        }//check grade in range
         else if (grade < MIN_GRADE || grade > MAX_GRADE) {
             fprintf(stderr, "Error at line %d: grade %d invalid\n", line, grade);
             exit(1);
-        }
+        }//update min grade
         else if(grade < min){
             min = grade;
         }
         line++;
 
-    }
-    printf("%d\n", min);
+    }//output minimal grade
+    printf("%d\t", min);
 }
