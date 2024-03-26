d64713695a21a4480438439565fac026ae21c8ae
YanShahar <158297968+YanShahar@users.noreply.github.com>
2024-03-26 20:25:24 +0200

    Update firewall.sh

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#Cleaning the rules file from unnecesery elements.
+clean_rule_file=$(cat $1 | sed '/^$/d' 
+                  | sed 's/#.*//' | sed 's/ //g')
+
+#We create an array to which we will add the relevant packets.
+rel_packets=()
+
+#We keep the input in a variable.
+packets=($cat);
+
+#Add a loop that checks if a rule is taken
+
+#Sort and uniq the final array
