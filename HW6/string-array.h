7b454f63d10325cef5bb7c5b21c0d9ebfbde8f9e
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-27 23:08:09 +0200

    Update string-array.h

--- HW6/string-array.h
+++ HW6/string-array.h
@@ -1,1 +1,38 @@
 
+#ifndef STRINGARRAY_H
+#define STRINGARRAY_H
+
+#include "string.h"
+#include "generic-string.h"
+#include <cstring>
+
+class StringArray {
+private:
+    String* array; // Array of String objects
+    size_t elements;   // Number of elements in the array
+
+public:
+    // Constructors and destructor
+    StringArray();
+    StringArray(size_t initialSize);
+    StringArray(const StringArray& other);
+    ~StringArray();
+
+    // Accessors
+    size_t getSize() const;
+
+    // Element access
+    String& operator[](size_t index);
+    const String& operator[](size_t index) const;
+    /*
+    // Modifiers
+    void resize(size_t newSize);
+    void push_back(const String& str);
+    void clear();
+
+    // Utility functions
+    void print() const;
+     */
+};
+
+#endif // STRINGARRAY_H
