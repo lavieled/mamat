3dd305859cd8f44232d04f46af46171785716bea
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-28 00:25:17 +0200

    Update string.cpp

--- HW6/string.cpp
+++ HW6/string.cpp
@@ -1,8 +1,9 @@
-//
-// Created by llede on 25/03/2024.
+//Lavie Lederman 319046504
+//Shahar Yankelovich 206159527
 //
 #include "generic-string.h"
 #include "string.h"
+#include "string_array.h"
 #include <iostream>
 #include <string.h>
 #include <cstring>
@@ -10,7 +11,7 @@ using std::cout;
 using std::endl;
 
 
-//global functions
+//global function
 GenericString* make_string(const char *str) {
     return new String(str); // Create a String object using the provided C-style string
 }
@@ -40,7 +41,7 @@ String::String(const char *str){
 String::~String(){
         delete[] data;//free memory
 }
-
+//assigment operators
 String& String::operator=(const String &rhs){
     if(this != &rhs){
         delete[] data; // Free memory
@@ -57,7 +58,7 @@ String& String::operator=(const char *str){
     strcpy(data, str);// Copy data
     return *this;
 }
-
+//equal operators
 bool String::operator==(const GenericString &rhs) const{
 
     if(length == rhs.as_string().length){
@@ -76,13 +77,23 @@ bool String::operator==(const char *str) const{
     }
     return false;
 }
-StringArray String::split(
-        const char *delimiters, String **output, size_t *size) const {
+//split function turning string into array of substrings divided by delimiters
+StringArray String::split(const char *delimiters) const {
+    StringArray result;//create array
+    char *token = strtok(data, delimiters);//use strtok to find delimiters
+    while (token != nullptr) {
+        result.push_back(String(token));//add new string to array
+        token = strtok(nullptr, delimiters);//search for next string
+    }
+    //run until done, return string array
+    return result;
 
 }
+//return number string as integer
 int String::to_integer() const {
     return atoi(data);
 }
+//cut out empty spaced on the outside of strings
 String& String::trim(){
     int left = 0;
     int right = length - 1;
@@ -95,21 +106,23 @@ String& String::trim(){
         return *this;
     }
 
-    //trailing white spaces
+    //right white spaces
     while(data[right] == ' ') {
         right--;
     }
-
-    int new_size = right - left + 2;
-    char *new_data = new char[new_size];
+    //calc new string length
+    int new_length = right - left + 2;
+    char *new_data = new char[new_length];
     int j = 0;
+    //copy only relevant part of string
     for(int i = left; i<=right; i++){
         new_data[j] = data[i];
         j++;
     }
     new_data[j] = '\0';
+    //update string to leaner version, delete temp memory
     delete[] data;
-    length = new_size;
+    length = new_length;
     data = new char[length + 1];// Allocate memory
     strcpy(data, new_data);// Copy data
     delete[] new_data;
@@ -117,11 +130,12 @@ String& String::trim(){
     return *this;
 
 }
-
+//casts generic-string into string
 String& String::as_string(){
-
+    String& str = dynamic_cast<String&>(*this);
+    return str;
 }
 String& String::as_string() const{
-
+    String& str = const_cast<String&>(*this);
+    return str;
 }
-
