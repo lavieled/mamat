98606ea3ac6d7622b0395765c969ed79ed392292
YanShahar <158297968+YanShahar@users.noreply.github.com>
2024-03-27 15:30:39 +0200

    Update ip.h

--- HW6/ip.h
+++ HW6/ip.h
@@ -1,1 +1,20 @@
+#ifndef IP_H
+#define IP_H
 
+//Maybe add libraries?
+#include "generic-field.h"
+//ip is a class inheriting from GenericField.
+class ip : public GenericField {
+  string ip_name;
+  unsigned int mask_ip;
+  int mask_num;
+
+public :
+  ip(): ip_name(nullptr), mask_ip(0), mask_nun(0){}
+  ip(const string &ip);
+  bool set_value(string value);
+  bool match(string packet);
+  ~ip()
+};
+
+#endif
