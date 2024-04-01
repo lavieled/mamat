1e6f3530751108976b4453686241f8b597e390a4
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-01 17:19:48 +0300

    Update main.cpp

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -1,3 +1,30 @@
-wakef wakef banana batahat
-printf("hello world!");
-hi shahar :)
+#include <iostream>
+#include "input.h"
+#include "string.h"
+#include "port.h"
+#include "ip.h"
+#include "string_array.h"
+
+
+int main(int argc, char **argv){
+    //check input???
+    String input(argv[1]);
+    StringArray rule;
+    //read rule
+    rule = input.split("=");
+    String name = (rule[0]).trim();
+    String val = rule[1];
+    if(name == "src-ip" || name == "dst-ip"){
+        ip main_ip(name);
+        main_ip.set_value(val);
+        parse_input(main_ip);
+    }
+
+    if(name == "src-port" || name == "dst-port"){
+        port main_port(name);
+        main_port.set_value(val);
+        parse_input(main_port);
+
+    }
+    return 0;
+}//shahar didnt even see my last joke:(
