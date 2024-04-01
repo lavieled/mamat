2cc6c9fb070d684d8c0043b2aedbe2c7ce1dbf0d
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-01 17:49:51 +0300

    Update firewall.sh

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-#Cleaning the rules file from unnecesery elements.
-clean_rule_file=$(cat $1 | sed '/^$/d' 
-                  | sed 's/#.*//' | sed 's/ //g')
+if [[ $# -ne 1 ]]; then
+    echo "Wrong number of arguments" >&2
+    exit 1
+fi
 
-#We create an array to which we will add the relevant packets.
-rel_packets=()
+#cleans rules file
+rule_file=$(cat $1 | sed 's/ //g' | \
+                sed 's/#.*//' | \
+                sed '/^$/d' | \
+                sed 's/,\{2,\}/,/g' | \
+                sed 's/^,//g')
 
-#We keep the input in a variable.
-packets=($cat);
+##stores input packets into a variable
+packets=$(cat);
 
-#Add a loop that checks if a rule is taken
+# Array to store the valid packets
+valid_packets=()
 
-#Sort and uniq the final array
+while read -r complex_rule; do
+
+    IFS=',';
+
+    read -ra rules <<< "$complex_rule"
+
+    # Check if the line passes all four sub-rules
+    valid_packets+=$(echo "$packets" | ./firewall.exe "${rules[0]}" | \
+                                      ./firewall.exe "${rules[1]}" | \
+                                      ./firewall.exe "${rules[2]}" | \
+                                      ./firewall.exe "${rules[3]}" )
+    valid_packets+=$'\n'
+
+done <<< "$rule_file"
+
+# Sort the valid packets and remove duplicates
+sorted_unique_packets=$(echo "$valid_packets" | sort | uniq | sed 's/ //g' | sed '/^$/d' )
+
+# Print the sorted and unique packets
+echo "$sorted_unique_packets"
