fdcea95699133ed4a60e579d1c6092cabc442faf
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-01 17:16:42 +0300

    Update port.h

--- HW6/port.h
+++ HW6/port.h
@@ -15,13 +15,12 @@
 class port : public GenericField{
     unsigned short min_val;
     unsigned short max_val;
-
     String port_name;
 
 public:
     port(): min_val(MINPORT), max_val(MAXPORT), port_name(nullptr){}
     port(const String &Port);
-    bool match(String packet);
+    bool match(const GenericString& packet) const;
     bool set_value(String value);
     ~port();
 
