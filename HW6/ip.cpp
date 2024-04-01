8f839a24c62ff903ffca17a232d998cc5b8b3136
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-01 17:17:30 +0300

    Update ip.cpp

--- HW6/ip.cpp
+++ HW6/ip.cpp
@@ -1,39 +1,72 @@
 //Lavie Lederman 319046504
 //Shahar Yankelovich 206159527
 
-//Maybe add a iostream or string library?
+
 #include "ip.h"
 
-#define MAXMASK 32
-#define TOTBYTES 4
+#define IPBYTES 4
 #define SHIFT 8
+#define MASKMAX 32
 
 // Constructor definition
-ip::ip(const string &ip) : GenericField(){
+ip::ip(const String &ip) : GenericField(){
     ip_name = ip;
     mask_ip = 0;
     mask_num = 0;
 }
 
+ip::~ip(){}
+
 // set_value function definition
-bool ip::set_value(std::string value) {
-    // Set the value of the IP (assuming value represents the IP address)
-    size_t pos = value.find('/');
-    //Check if the received position is valid.
-    if(pos == std::string::npos) return false;
-    ip_name = value.substr(0, pos);
-    mask_num - std::stoi(value.substr(pos+1);
-    return true; // Return true to indicate success
+unsigned int ip::ip_to_int(String &ip){
+    StringArray ip_bytes;
+    ip = ip.trim();
+    ip_bytes = ip.split(".");
+    unsigned int ip_int = 0;
+    int curr;
+    for (int i = 0; i < IPBYTES ; i++ ) {
+        curr = ip_bytes[i].to_integer();
+        ip_int += (curr << ((3 - i) * SHIFT));
+        //ip_int |= (curr << ((3 - i) * SHIFT));
+    }
+    return ip_int;
 }
 
-// match function definition
-bool ip::match(std::string packet) {
-   
-    return (packet_ip_int == mask_ip);
-}
+bool ip::set_value(String value) {
+    StringArray ip_mask;
+    ip_mask = value.split("/");//split values
+    unsigned int int_ip = ip_to_int(ip_mask[0]);//the ip
+    unsigned int mask = ip_mask[1].to_integer();//mask num
+    //checks that we got a valid mask number
+    if(mask < 0 || mask > MASKMAX){
+        return false;
+    }
+    //update ip according to mask
+    mask_num = mask;
+    mask_ip = int_ip >> (MASKMAX - mask_num);
+    return true;
 
-// Destructor definition
-ip::~ip() {
-    // Destructor typically does not require explicit implementation
-    // However, if there are resources to clean up, you can do so here
+}
+bool ip::match(const GenericString& packet) const {
+    String const &packet_str = packet.as_string();
+    StringArray packet_split, ip_addrs;
+    if(packet == ""){
+        return false;
+    }//check for empty string
+    //split to fields
+    packet_split = packet_str.split(",");
+    for(int i=0; i < packet_split.getSize(); i++){
+        packet_split[i] = packet_split[i].trim();
+        //split to name & value
+        ip_addrs = packet_split[i].split("=");
+        ip_addrs[0] = ip_addrs[0].trim();
+        if(ip_addrs[0] == ip_name){
+            unsigned int curr_ip = ip_to_int(ip_addrs[1]);
+            //remove the unwanted number of bits
+            curr_ip = curr_ip >> (MASKMAX - mask_num);
+            //returns true if the ip number meets the rule
+            return (curr_ip == mask_ip);
+        }
+    }
+    return false;
 }
