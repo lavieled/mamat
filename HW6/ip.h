cbf3319dcf4df3123c92b78936a9ba89aa24ebd3
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-01 17:17:03 +0300

    Update ip.h

--- HW6/ip.h
+++ HW6/ip.h
@@ -1,20 +1,22 @@
 #ifndef IP_H
 #define IP_H
 
-//Maybe add libraries? string library for std::string in line 8
+#include "string_array.h"
+#include "string.h"
 #include "generic-field.h"
 //ip is a class inheriting from GenericField.
 class ip : public GenericField {
-  string ip_name;
-  unsigned int mask_ip;
-  int mask_num;
+    String ip_name;
+    unsigned int mask_ip;
+    int mask_num;
 
 public :
-  ip(): ip_name(nullptr), mask_ip(0), mask_num(0){}
-  ip(const string &ip);
-  bool set_value(string value);
-  bool match(string packet);
-  ~ip();
+    ip(): ip_name(nullptr), mask_ip(0), mask_num(0){}
+    ip(const String &ip);
+    bool set_value(String value);
+    unsigned int ip_to_int(String &ip);
+    bool match(const GenericString& packet) const;
+    ~ip();
 };
 
 #endif
