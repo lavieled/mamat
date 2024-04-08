b69b442eaf44f4d6ed37a6bfdb3a20389aa09b15
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-08 16:16:44 +0300

    Update main.cpp

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 #include "input.h"
+#include "generic-field.h"
+#include "generic-string.h"
 #include "string.h"
 #include "port.h"
 #include "ip.h"
 #include "string-array.h"
 
 
-int main(int argc, char **argv){
-    //check input???
+int main(int argc, char **argv) {
+ 
+    if(argc != 2){
+        return 1;
+    }
     String input(argv[1]);
-    StringArray rule;
-    //read rule
-    rule = input.split("=");
-    String name = (rule[0]).trim();
-    String val = rule[1];
-    if(name == "src-ip" || name == "dst-ip"){
+    
+    //String input("src-ip=241.1.205.106/32");
+    StringArray rule = input.split("=");
+
+    if (rule.getSize() < 2) {
+        std::cerr << "Invalid input format: " << std::endl;
+        return 1;
+    }
+
+    String name = rule[0].trim();
+    String val = rule[1].trim();
+
+    if (name == "src-ip" || name == "dst-ip") {
         ip main_ip(name);
         main_ip.set_value(val);
         parse_input(main_ip);
     }
 
-    if(name == "src-port" || name == "dst-port"){
+    if (name == "src-port" || name == "dst-port") {
         port main_port(name);
         main_port.set_value(val);
         parse_input(main_port);
-
     }
+
     return 0;
-}//shahar didnt even see my last joke:(
+}
