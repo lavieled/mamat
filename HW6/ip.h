53b6c868d92a06af650a1f8d100fc1c195c50c26
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-09 13:27:03 +0300

    Update ip.h

--- HW6/ip.h
+++ HW6/ip.h
@@ -1,9 +1,13 @@
 #ifndef IP_H
 #define IP_H
 
+#include <stddef.h>
+#include <iostream>
+#include "generic-string.h"
+#include "generic-field.h"
 #include "string-array.h"
 #include "string.h"
-#include "generic-field.h"
+
 //ip is a class inheriting from GenericField.
 class ip : public GenericField {
     String ip_name;
@@ -11,10 +15,10 @@ class ip : public GenericField {
     int mask_num;
 
 public :
+
     ip(): ip_name(nullptr), mask_ip(0), mask_num(0){}
-    ip(const String &ip);
+    ip(const String& ip);
     bool set_value(String value);
-    unsigned int ip_to_int(String &ip) const;
     bool match(const GenericString& packet) const;
     ~ip();
 };
