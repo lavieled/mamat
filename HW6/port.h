7c8e8346249016b115646b28ac2c29bb4f0ebf46
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-09 13:28:29 +0300

    Update port.h

--- HW6/port.h
+++ HW6/port.h
@@ -1,6 +1,5 @@
-//Lavie Lederman 319046504
-//Shahar Yankelovich 206159527
-//
+// Lavie Lederman 319046504
+// Shahar Yankelovich 206159527
 
 #ifndef PORT_H
 #define PORT_H
@@ -8,22 +7,24 @@
 #define MINPORT 0
 #define MAXPORT 65535
 
+#include "generic-string.h"
+#include "generic-field.h"
 #include "string-array.h"
 #include "string.h"
-#include "generic-field.h"
 
-class port : public GenericField{
+
+class port : public GenericField {
+public:
     String port_name;
     unsigned short min_val;
     unsigned short max_val;
 
-public:
-    port(): port_name(nullptr), min_val(MINPORT), max_val(MAXPORT){}
-    port(const String &Port);
-    bool match(const GenericString& packet) const;
+    port() : port_name(nullptr), min_val(MINPORT), max_val(MAXPORT) {}
+    port(const String& Port);
     bool set_value(String value);
+    bool match(const GenericString& packet) const;
+    
     ~port();
-
-
 };
-#endif //PORT_H
+
+#endif // PORT_H
