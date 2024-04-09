70762b5f2e80a46e2d84d57603722bd7e50af300
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-09 13:31:19 +0300

    Update string.cpp

--- HW6/string.cpp
+++ HW6/string.cpp
@@ -1,142 +1,132 @@
-//Lavie Lederman 319046504
-//Shahar Yankelovich 206159527
-//
-#include "generic-string.h"
+// lavie lederman 319046504
+// shahar yankelovich 206159527
+
+
 #include "string.h"
 #include "string-array.h"
-#include <iostream>
-#include <string.h>
-#include <cstring>
-using std::cout;
-using std::endl;
+#include <cstring> // Include <cstring> for strcpy, strlen, and memmove
+#include <cctype>  // Include <cctype> for isspace
+#include <cstdlib> // Include <cstdlib> for atoi
 
-
-//global function
-GenericString* make_string(const char *str) {
-    return new String(str); // Create a String object using the provided C-style string
-}
-
-//constructors and destructor
-String::String() {
-    data = new char [1];
-    data[0] = '\0';
-    length = 0;
-}
-
-String::String(const String &str){
-   if(str.data == NULL){
-        return;
-    }
-    data = new char[str.length + 1];// Allocate memory
-    length = str.length;// Copy length
-    strcpy(data, str.data);// Copy data
-}
-
-String::String(const char *str){
-    length = strlen(str);// Copy length
-    data = new char[length + 1];// Allocate memory
-    strcpy(data, str);// Copy data
-}
-
-String::~String(){
-        delete[] data;//free memory
-}
-
-//assigment operators
-String& String::operator=(const String &rhs){
-    if(this != &rhs){
-        delete[] data; // Free memory
-        length = rhs.length; // Copy length
-        data = new char[length + 1]; // Allocate memory
-        strcpy(data, rhs.data); // Copy data
-    }
-    return *this;
-    }
-
-String& String::operator=(const char *str){
+// Constructor
+String::String(const char* str) {
     length = strlen(str);
-    data = new char[length + 1];// Allocate memory
-    strcpy(data, str);// Copy data
+    data = new char[length + 1]; // Allocate memory
+    strcpy(data, str); // Copy data
+}
+
+// Copy constructor
+String::String(const String& other) {
+    length = other.length;
+    data = new char[length + 1]; // Allocate memory
+    strcpy(data, other.data); // Copy data
+}
+
+// Destructor
+String::~String() {
+    if (data != nullptr) {
+        delete[] data;
+    }
+}
+
+
+// Implement methods from GenericString interface
+StringArray String::split(const char* delimiters) const {
+    // Implement split logic
+    StringArray result;
+    char* token = strtok(data, delimiters);
+    while (token != nullptr) {
+        result.add(make_string(token));
+        token = strtok(nullptr, delimiters);
+    }
+    return result;
+}
+
+GenericString& String::operator=(const char* str) {
+    // Calculate the length of the new string
+    size_t newLength = strlen(str);
+
+    // If the new string length is the same as the current one, just copy the data
+    if (newLength == length) {
+        strcpy(data, str);
+    } else {
+        // Allocate new memory for the new string
+        char* newData = new char[newLength + 1];
+        strcpy(newData, str);
+
+        // If there was already allocated memory for data, delete it
+        if (data != nullptr) {
+            delete[] data;
+        }
+
+        // Update the data pointer and length
+        data = newData;
+        length = newLength;
+    }
+
     return *this;
 }
-//equal operators
-bool String::operator==(const GenericString &rhs) const{
 
-    if(length == rhs.as_string().length){
-        if( strcmp(data, rhs.as_string().data) ){
-            return true;
-        }
-    }
-    return false;
-}
-
-bool String::operator==(const char *str) const{
-    if(length == strlen(str)){
-        if( strcmp(data, str) ){
-            return true;
-        }
-    }
-    return false;
-}
-//split function turning string into array of substrings divided by delimiters
-StringArray String::split(const char *delimiters) const {
-    StringArray result;//create array
-    char *token = strtok(data, delimiters);//use strtok to find delimiters
-    while (token != nullptr) {
-        result.push_back(String(token));//add new string to array
-        token = strtok(nullptr, delimiters);//search for next string
-    }
-    //run until done, return string array
-    return result;
-
-}
-//return number string as integer
-int String::to_integer() const {
-    return atoi(data);
-}
-//cut out empty spaced on the outside of strings
-String& String::trim(){
+GenericString& String::trim() {
     int left = 0;
     int right = (int)length - 1;
-    //left white spaces
-    while(data[left] == ' '){
+
+    // Remove leading whitespace
+    while (left <= right && (data[left] == ' ' || data[left] == '\t' || data[left] == '\n' || data[left] == '\r' || data[left] == '\f' || data[left] == '\v')) {
         left++;
     }
 
-    if(left ==(int)length){
+    // Check if the entire string is whitespace
+    if (left > right) {
+        length = 0;
+        //delete[] data;
+        data = new char[1];
+        data[0] = '\0';
         return *this;
     }
 
-    //right white spaces
-    while(data[right] == ' ') {
+    // Remove trailing whitespace
+    while (right >= left && (data[right] == ' ' || data[right] == '\t' || data[right] == '\n' || data[right] == '\r' || data[right] == '\f' || data[right] == '\v')) {
         right--;
     }
-    //calc new string length
-    int new_length = right - left + 2;
-    char *new_data = new char[new_length];
-    int j = 0;
-    //copy only relevant part of string
-    for(int i = left; i<=right; i++){
-        new_data[j] = data[i];
-        j++;
+
+    // Calculate new string length
+    length = right - left + 1;
+
+    // Shift content to the beginning of the string if needed
+    if (left != 0) {
+        memmove(data, data + left, length);
     }
-    new_data[j] = '\0';
-    //update string to leaner version, delete temp memory
-    delete[] data;
-    length = (size_t)new_length;
-    data = new char[length + 1];// Allocate memory
-    strcpy(data, new_data);// Copy data
-    delete[] new_data;
+
+    // Null-terminate the string
+    data[length] = '\0';
 
     return *this;
+}
 
+
+bool String::operator==(const GenericString& other) const {
+    const String& otherString = static_cast<const String&>(other);
+    return strcmp(data, otherString.data) == 0;
 }
-//casts generic-string into string
-String& String::as_string(){
-    String& str = dynamic_cast<String&>(*this);
-    return str;
+
+bool String::operator==(const char* other) const {
+    return strcmp(data, other) == 0;
 }
-String& String::as_string() const{
-    String& str = const_cast<String&>(*this);
-    return str;
+
+int String::to_integer() const {
+    return atoi(data);
+}
+
+String& String::as_string() {
+    return *this;
+}
+
+const String& String::as_string() const {
+    return *this;
+}
+
+// Global method implementation
+GenericString* make_string(const char* str) {
+    return new String(str);
 }
