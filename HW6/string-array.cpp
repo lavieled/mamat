4c0c4a8e493b806cc3d10664c802fcd1cc5c60e6
lavieled <158300455+lavieled@users.noreply.github.com>
2024-04-09 15:22:08 +0300

    Update string-array.cpp

--- HW6/string-array.cpp
+++ HW6/string-array.cpp
@@ -6,12 +6,10 @@ StringArray::StringArray() : strings(nullptr), size(0) {}
 // Destructor
 StringArray::~StringArray() {
     // Deallocate memory for each GenericString object in the array
-    /*for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i) {
         delete strings[i];
     }
     // Deallocate memory for the array itself
-    delete[] strings;
-    */
     if (strings != nullptr) {
             delete[] strings;
         }
@@ -20,14 +18,21 @@ StringArray::~StringArray() {
 // Add a string to the array
 void StringArray::add(GenericString* str) {
     // Increase size of array
-    GenericString** temp = new GenericString*[size + 1];
+    GenericString** temp = new GenericString*[size + 1]; // Allocate memory for new array
+
     for (size_t i = 0; i < size; ++i) {
-        temp[i] = strings[i];
+        temp[i] = strings[i]; // Copy old pointers to the new array
     }
-    temp[size++] = str;
+
+    temp[size++] = str; // Add the new string pointer to the new array
+
+    // Deallocate memory for the old array
+    delete[] strings;
+
     // Update array pointer to point to the new array
     strings = temp;
 }
+
 
 // Operator to access individual strings
 GenericString& StringArray::operator[](size_t index) {
